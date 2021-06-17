a4b79fcca4fc2c269a17eb4fc085229004cd5233
amyvelasquez <velasquezamy213@gmail.com>
2021-06-16 20:03:28 -0400

    I've entered solar sys feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+        //write your code here
+
+        SolarSystem xfac = new SolarSystem();
+        xfac.diameter="3456543";
+        xfac.name="Pynk";
+        xfac.size="8922miles";
+        xfac.color="pink";
     }
 }
---
+++ src/SolarSystem.java
@@ -0,0 +1,7 @@
+public class SolarSystem {
+
+    String diameter;
+    String name;
+    String color;
+    String size;
+}
